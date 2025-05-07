23bfd0fdd60b73887702a33d72aaba42005c18e3
KINNONG <1044869436@qq.com>
2025-05-07 14:11:08 +0800

    feat: 修改了首页和商店页面的图片

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,13 @@ import CategoriesSection from "@/components/sub/CategoriesSection";
 import FaqSection from "@/components/sub/FaqSection";
 import FeatureSection from "@/components/sub/FeatureSection";
 import ShowcaseSection from "@/components/sub/ShowcaseSection";
-import Watermark from "@/components/sub/Watermark";
 import WorkflowSection from "@/components/sub/WorkflowSection";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
-        <Watermark text="程序员小白龙" />
+        {/* <Watermark text="程序员小白龙" /> */}
         <Hero />
         <Skills />
         <WorkflowSection />
